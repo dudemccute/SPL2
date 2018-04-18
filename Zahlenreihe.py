89691125bdde3f28e0771bd3cb3b2c0a56617548
dudemccute <171witb15@o365.htl-leoben.at>
2018-04-18 11:50:10 +0200

    Zahlenreihe.py

--- Zahlenreihe.py
+++ Zahlenreihe.py
@@ -1,10 +1,16 @@
 #Zahlenreihe.py
 
-n = input("Bitte n eingeben")
-n = int(n)
-
+#n = input("Bitte n eingeben")
+#n = int(n)
+n = 1000
 for i in range (1,n+1):
     if(i < n):
      print(i, end= " < ")
     else:
         print(i)
+
+#Alle geraden Zahlen von 1..n ausgebe
+for i in range (1,n+1):
+    if (i % 2 == 0):
+        print(i , end = ";")
+   
