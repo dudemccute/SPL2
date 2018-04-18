1cd33a257b27a1bbce8e3d5fb6342a38dc9e475e
dudemccute <171witb15@o365.htl-leoben.at>
2018-04-18 12:06:01 +0200

    Zahlenreihe.py

--- Zahlenreihe.py
+++ Zahlenreihe.py
@@ -8,14 +8,22 @@ for i in range (1,n+1):
      print(i, end= " < ")
     else:
         print(i)
-
+print("\n")
 #Alle geraden Zahlen von 1..n ausgebe
 for i in range (1,n+1):
     if (i % 2 == 0):
         print(i , end = " ; ")
 
-
+print("\n")
 #Alle ungeraden
 for i in range (1,n+1):
     if (i % 2 != 0):
         print(i , end = " ; ")
+print("\n")
+#Durch 9
+for i in range (1,n+1):
+    if (i % 9 == 0):
+        print(i , end = " ; ")
+print("\n")
+
+
