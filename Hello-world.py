23104a70a4b4580791a8d66772ac3e05d4f32ba5
dudemccute <171witb15@o365.htl-leoben.at>
2018-04-18 10:19:11 +0200

    Hello-world.py

--- Hello-world.py
+++ Hello-world.py
@@ -1,9 +1,10 @@
 print("Hallo, ich bin ein computer")
 name = input ("Wie lautet dein Name?")
-print  ("Schön dich zu treffen", name,)
+print  ("Schön dich zu treffen", name)
 print  ("Dein Name enthält",len(name),"Zeichen")
-alter =(input ("Wie alt bist du?")
+alter =input ("Wie alt bist du?")
 alter = int(alter)
+
 if(alter > 100):
     print("Das ist wohl ein Scherz oder")
 else:
