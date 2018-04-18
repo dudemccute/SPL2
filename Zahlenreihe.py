23b2fd6db8b45d92ff6291f98e3baf4e1ed9e5ff
dudemccute <171witb15@o365.htl-leoben.at>
2018-04-18 11:56:25 +0200

    Zahlenreihe.py

--- Zahlenreihe.py
+++ Zahlenreihe.py
@@ -12,5 +12,10 @@ for i in range (1,n+1):
 #Alle geraden Zahlen von 1..n ausgebe
 for i in range (1,n+1):
     if (i % 2 == 0):
-        print(i , end = ";")
-   
+        print(i , end = " ; ")
+
+
+#Alle ungeraden
+for i in range (1,n+1):
+    if (i % 2 != 0):
+        print(i , end = " ; ")
